90c36b196abb89cbe973cb21e5e1304e859ab8ca
Obanai2M <166419379+Obanai2M@users.noreply.github.com>
2024-04-15 01:22:48 +0530

    Update vanako.py

--- NekoRobot/modules/vanako.py
+++ NekoRobot/modules/vanako.py
@@ -33,10 +33,10 @@ PHOTO = "https://te.legra.ph/file/5cdc460a2ed69abcbee60.gif"
 
 @register(pattern=("Welcome"))
 async def awake(event):
-    NEKO = f" ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴏᴜʀ ᴋɪɴɢᴅᴏᴍ♡︎ ɪ ʜᴏᴘᴇ ʏᴏᴜ ᴄᴀɴ ɢᴇᴛ ᴍᴀɴʏ ғʀɪᴇɴᴅs ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ"
+    NEKO = f" WELCOME TO OUR CASTLE♡︎ ɪ ʜᴏᴘᴇ ʏᴏᴜ ᴄᴀɴ ɢᴇᴛ ᴍᴀɴʏ ғʀɪᴇɴᴅs ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ"
     BUTTON = [
         [
-            Button.url("Meet Me Here🎀", "https://telegram.dog/Besties_XD"),
+            Button.url("Meet Me Here🎀", "https://t.me/Ahjin_sprt"),
         ]
     ]
     await tbot.send_file(event.chat_id, PHOTO, caption=NEKO, buttons=BUTTON)
