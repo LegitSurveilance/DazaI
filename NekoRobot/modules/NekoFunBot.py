f92ed4489426880d18e442f24944801247d66d4c
Obanai2M <166419379+Obanai2M@users.noreply.github.com>
2024-04-15 13:18:02 +0530

    Update NekoFunBot.py

--- NekoRobot/modules/NekoFunBot.py
+++ NekoRobot/modules/NekoFunBot.py
@@ -7,11 +7,11 @@ from NekoRobot import NEKO_PTB
 from NekoRobot.modules.disable import MessageHandler
 
 OWO = (
-    "*Obanai pats {} on the head.",
+    "*Dazai pats {} on the head.",
     "*gently rubs {}'s head*.",
-    "*Obanai mofumofus {}'s head*",
-    "*Obanai messes up {}'s head*",
-    "*Obanai intensly rubs {}'s head*",
+    "*Dazai mofumofus {}'s head*",
+    "*Dazai messes up {}'s head*",
+    "*Dazai intensly rubs {}'s head*",
     "*{}'s waifu pats their head*",
     "*{}'s got free headpats*",
     "No pats for {}!",
